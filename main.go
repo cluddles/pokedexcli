fe22f7bcdbdef0b29d262b451af5e9cf29604aaa
Dan Fielding <dgfielding@gmail.com>
2025-01-06 21:34:42 +0000

    simple REPL

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		fields := cleanInput(str)
+		if len(fields) > 0 {
+			fmt.Printf("Your command was: %s\n", fields[0])
+		}
+	}
 }
